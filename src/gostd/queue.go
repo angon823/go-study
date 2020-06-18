829a6154db257905e24bd50fe28f0d7d2132b2c6
dayong <jiangyong9497@163.com>
2020-06-18 11:57:02 +0800

    添加queu实现

--- src/gostd/queue.go
+++ src/gostd/queue.go
@@ -7,3 +7,35 @@ package gostd
 * @Description:
 
 **/
+
+type Queue struct {
+	deque *Deque
+}
+
+func NewQueue() *Queue {
+	return &Queue{deque: NewDeque()}
+}
+
+func (q *Queue) Push(val interface{}) {
+	q.deque.PushBack(val)
+}
+
+func (q *Queue) Pop() interface{} {
+	return q.deque.PopFront()
+}
+
+func (q *Queue) Front() interface{} {
+	return q.deque.Front()
+}
+
+func (q *Queue) Back() interface{} {
+	return q.deque.Back()
+}
+
+func (q *Queue) Size() int {
+	return q.deque.Size()
+}
+
+func (q *Queue) Empty() bool {
+	return q.deque.Size() == 0
+}
