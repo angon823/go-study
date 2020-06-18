a00254cf0cf9ce262f78e37cd38492dade057a7c
dayong <jiangyong9497@163.com>
2020-06-18 11:57:12 +0800

    添加栈实现

--- src/gostd/stack.go
+++ src/gostd/stack.go
@@ -8,4 +8,29 @@ package gostd
 **/
 
 type Stack struct {
+	deque *Deque
+}
+
+func NewStack() *Stack {
+	return &Stack{deque: NewDeque()}
+}
+
+func (s *Stack) Push(val interface{}) {
+	s.deque.PushFront(val)
+}
+
+func (s *Stack) Pop() interface{} {
+	return s.deque.PopFront()
+}
+
+func (s *Stack) Top() interface{} {
+	return s.deque.Front()
+}
+
+func (s *Stack) Size() int {
+	return s.deque.Size()
+}
+
+func (s *Stack) Empty() bool {
+	return s.deque.Size() == 0
 }
